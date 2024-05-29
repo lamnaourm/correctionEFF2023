9bbc41a24c6de16ed98bc3efa5e327c338c036ef
lamnaourm <mohammed.lamnaour@gmail.com>
2024-05-29 14:51:05 +0100

    2

--- src/components/Composant2.jsx
+++ src/components/Composant2.jsx
@@ -14,7 +14,21 @@ export default function Composant2() {
     }, [])
   return (
     <div>
-        
+        <h1>Liste des Salariés</h1>
+        <table>
+            <tr>
+                <th>nom</th>
+                <th>prenom</th>
+                <th>fonction</th>
+                <th>service</th>
+            </tr>
+            {salaries.map(s => <tr>
+                <td>{s.nomsal}</td>
+                <td>{s.prenomsal}</td>
+                <td>{s.fonction}</td>
+                <td>{s.service.nomser}</td>
+            </tr>)}
+        </table>
     </div>
   )
 }
