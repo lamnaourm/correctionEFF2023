f38651d8bf61a484b85cd52b62806e1a026964f2
lamnaourm <mohammed.lamnaour@gmail.com>
2024-05-29 15:18:52 +0100

    q4

--- src/components/Composant3.jsx
+++ src/components/Composant3.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Composant3({salaries}) {
-  return (
-    <div>
-        
-    </div>
-  )
+export default function Composant3({ salaries }) {
+    const [filter, setFilter] = useState()
+    const [salariesFiltred, setSalariesFiltred] = useState([])
+
+    const handleClick = () => {
+        setSalariesFiltred(salaries.filter(s => s.service.nomser == filter))
+    }
+
+    return (
+        <div>
+            <h2>Recherche par service : </h2>
+            Entrer le nom du service :
+            <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+            <input type="button" onClick={handleClick} />
+            <h2>Resultat :</h2>
+            <ul>
+                {salariesFiltred.map(s => <li>
+                    nom : {s.nomsal} Prenom : {s.prenomsal}
+                </li>)}
+            </ul>
+        </div>
+    )
 }
