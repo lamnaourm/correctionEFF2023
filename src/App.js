c23a3ddb7d4f26f21d0f99bb80e118333020e352
lamnaourm <mohammed.lamnaour@gmail.com>
2024-05-29 15:24:12 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Composant1 from './components/Composant1';
 import Composant2 from './components/Composant2';
 import Composant3 from './components/Composant3';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
@@ -18,9 +19,12 @@ function App() {
     }, [])
   return (
     <div>
-      <Composant1 />
-      <Composant2 salaries={salaries} />
-      <Composant3 salaries={salaries} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/q2' element={<Composant2 salaries={salaries} />} />
+          <Route path='/q3' element={<Composant3 salaries={salaries} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
